55e96e185d094e4620888e85b3f1ce5daec2a4e5
AvoDjan <119239831+AvoDjan@users.noreply.github.com>
2023-11-14 17:36:05 +0100

    Fin du CRUD

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+// On démarre une session
+session_start();
+
+// On inclut la connexion à la base
+require_once('connect.php');
+
+$sql = 'SELECT * FROM `liste`';
+
+// On prépare la requête
+$query = $db->prepare($sql);
+
+// On exécute la requête
+$query->execute();
+
+// On stock le résultat dans un tableau associatif
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+require_once('close.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des produits</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <section class="col-12">
+                <?php
+                    if(!empty($_SESSION['erreur'])){
+                        echo '<div class="alert alert-danger" role="alert">
+                        '. $_SESSION['erreur'].'
+                      </div>';
+                      $_SESSION['erreur'] = "";
+                    }
+                ?>
+                <h1>Liste des produits</h1>
+                <table class="table">
+                    <thead>
+                        <th>ID</th>
+                        <th>Produit</th>
+                        <th>Prix</th>
+                        <th>Nombre</th>
+                        <th>Actif</th>
+                        <th>Actions</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // On boucle sur la variable result
+                            foreach($result as $produit) {
+                            ?>
+                            <tr>
+                                <td><?= $produit['id'] ?></td>
+                                <td><?= $produit['produit'] ?></td>
+                                <td><?= $produit['prix'] ?></td>
+                                <td><?= $produit['nombre'] ?></td>
+                                <td><?= $produit['actif'] ?></td>
+                                <td><a href="disable.php?id=<?= $produit['id'] ?>">A/D</a> <a href="details.php?id=<?= $produit ['id'] ?>">Voir</a> <a href="edit.php?id=<?= $produit['id'] ?>">Modifier</a> <a href="delete.php?id=<?= $produit['id'] ?>">Supprimer</a></td>
+                            </tr>
+                            <?php
+                            }
+                        ?>
+                    </tbody>
+                </table>
+                <a href="add.php" class="btn btn-primary">Ajouter un produit</a>
+
+            </section>
+        </div>
+    </main>
+</body>
+</html>
